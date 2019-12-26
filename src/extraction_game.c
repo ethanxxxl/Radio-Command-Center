1f8612d562dc6cbad0ecaad76d4af68e07357a8e
Ethan Smith <ethansmith.dev@gmail.com>
2019-12-26 01:37:14 -0500

    Added lots of bells and whistles. currently some of the sleeps are commented out, because I don't want to wait forever while testing.

--- src/extraction_game.c
+++ src/extraction_game.c
@@ -14,6 +14,7 @@
 // for the command center, all it needs to do is change the current objective very x minutes
 void extraction_game(float mins, int max_turns)
 {
+	printf("game init\n");
 	// load in the locations
 	struct Location* locs;
 	int locs_size;
@@ -22,6 +23,32 @@ void extraction_game(float mins, int max_turns)
 	srandom(time(NULL));
 
 	int current_location;
+
+	// TODO add game start transmission and delay
+	
+	transmit("The game mode is Extraction. The game will start in 15 minutes", NULL);
+
+	//sleep(10*60);
+
+	transmit("The game will start in 5 minutes", NULL);
+
+	//sleep(4*60);
+
+	transmit("The game will start in 1 minute", NULL);
+
+	//sleep(30);
+
+	transmit("30 seconds", NULL);
+
+	//sleep(20);
+
+	for ( int i=10; i >= 0; i-- )
+	{
+		char c[5];
+		sprintf(c, "%d", i);
+		transmit(c, NULL);
+		usleep(500);
+	}
 
 	for ( int i=0; i < max_turns; i++ )
 	{
@@ -37,16 +64,48 @@ void extraction_game(float mins, int max_turns)
 			}
 		}
 
-		// TODO make this transmit the actual information
-		// transmit this
-		transmit(locs[current_location].name);
-		
-		// TODO add warnings, to warn of an incomming transmission.
-		// sleep for min minutes
-		sleep(mins);
+		int secs = mins * 60;
+
+		if ( i == 0 )
+			goto transmit;
+
+		if ( mins >= 7 )
+		{
+			printf("time is more than 7\n");
+			// 5 minute warning
+			sleep(secs - 5*60);
+			transmit("Warning, the location will be changing in 5 minutes", NULL);
+			
+			// 1 minute warning
+			sleep(4 * 60);
+			transmit("Warning, the location will be changing in 1 minute", NULL);
+
+			sleep(60);
+		}
+		else if ( mins >= 5 )
+		{
+			printf("5 <= time <=\n");
+			// 1 minute warning.
+			sleep(secs - 60);
+			transmit("Warning, the location will be changing in 1 minute", NULL);
+
+			sleep(60);
+		}
+		else
+		{
+			printf("time < 5\n");
+			// if the time is too short, then there is no warning
+			sleep(secs);
+		}
+
+		transmit:
+		if ( i == max_turns - 1 )
+			transmit("The game is now over", NULL);
+		else
+			transmit("The new location is: ", locs[current_location].name,
+					". It is located at: ", locs[current_location].x, ", ", locs[current_location].y, ".",
+					NULL);
 	}
 
-	transmit("The game is now over");
-
-	unload_locations(locs);
+	unload_locations(locs, locs_size);
 }
